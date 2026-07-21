df85d25d0e3488cfb46f1a65fcd18c8f7b0fbe12
yuriZzJaxx <132253210+yurizzjaxx@users.noreply.github.com>
2026-07-21 05:28:08 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
-var f = document.createElement("meta")
-f.setAttribute("property", "ob:image")
-f.content = "https://yurizzjaxx.github.io/media/src/furina.gif"
-document.head.appendChild(f)
+class _EmbedsHanddler {
+  constructor() {
+    this.m = document.createElement("meta");
+  }
+  title(text) {
+    this.m.setAttribute("property", "ob:title");
+    this.m.content = text;
+    document.head.appendChild(this.m);
+    return;
+  }
+  description(text) {
+    this.m.setAttribute("property", "ob:description");
+    this.m.content = text;
+    document.head.appendChild(this.m);
+    return;
+  }
+  imageUrl(url) {
+    this.m.setAttribute("property", "ob:image");
+    this.m.content = url;
+    document.head.appendChild(this.m);
+    return;
+  }
+  thumbnail(url) {
+    this.m.setAttribute("property", "ob:thumbnail");
+    this.m.content = url;
+    document.head.appendChild(this.m);
+    return;
+  }
+  color(col) {
+    this.m.setAttribute("property", "ob:theme-color");
+    this.m.content = col;
+    document.head.appendChild(this.m);
+    return;
+  }
+}
+function onEmbeds() {
+  return new _EmbedsHanddler()
+}
+
+
+
+onEmbeds().title("title text")
+onEmbeds().description("description text")
